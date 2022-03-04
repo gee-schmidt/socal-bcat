5a9258ab1a960939d41880849ed83cb327fc19f3
Greta M Schmidt <gmschmidt@ucdavis.edu>
2022-03-04 14:50:35 -0800

    starting script

--- scripts/Step 6 Encounter model runs - trap covs only.R
+++ scripts/Step 6 Encounter model runs - trap covs only.R
@@ -1,1 +1,6 @@
 ### Step 6 Detection Models - trapcovs only sel first 
+
+#setwd first
+setwd("D:/Socal Bobcat Reproducible Research Folder")
+
+load("./Processed Data/socalbobcat_sfssdf_LR_telemandcovs.rda")
