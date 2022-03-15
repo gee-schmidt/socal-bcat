4a977fedbda1ce472bf10758d384b03baa4588ec
gee-schmidt <greta.m.schmidt@gmail.com>
2022-03-15 13:16:40 -0700

    move modsel to diff script

--- scripts/Step 6 Encounter model runs - trap covs only.R
+++ scripts/Step 6 Encounter model runs - trap covs only.R
@@ -67,23 +67,3 @@ save(m0.R,m0.L,
 
 
 
-############Model Selection######################
-
-
-
-fl.L <- fitList.oSCR(list(m0.L,
-                          mp0cam.L,
-                          mp0season.L,
-                          mp0rec.L,
-                          mp0site.L), rename=TRUE) #rename=T adds sensible model names
-ms.L <- modSel.oSCR(fl.L)
-ms.L
-
-
-fl.R <- fitList.oSCR(list(m0.R,
-                          mp0cam.R,
-                          mp0season.R,
-                          mp0rec.R,
-                          mp0site.R), rename=TRUE) #rename=T adds sensible model names
-ms.R <- modSel.oSCR(fl.R)
-ms.R
