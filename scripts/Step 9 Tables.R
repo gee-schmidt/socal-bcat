5061f7929dacdfbf80bec7c33d0044b97bdf95af
gee-schmidt <greta.m.schmidt@gmail.com>
2022-03-28 17:48:03 -0700

    start table script

--- scripts/Step 9 Tables.R
+++ scripts/Step 9 Tables.R
@@ -1,1 +1,21 @@
 #Step 9 Tables for MS 
+
+#load oscr package and other packages
+library(oSCR)
+library(here)
+
+#this folder path gets from socal_bcat up 3 folders to the reproducible research overarching folders
+#(./../../../)
+
+#load models 
+load("./../../../Results/Model Outputs/Density Mods/socalbobcat_densitymodels_LR.rda")
+
+
+########LEFT########
+
+#Model selection table
+
+#Model coefficient table 
+
+#Bobcat density table not made in R 
+
